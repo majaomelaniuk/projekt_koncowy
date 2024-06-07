61de1800667e9f5fa8c1e3485b5a9c33c6d0ec60
majaomelaniuk <150002963+majaomelaniuk@users.noreply.github.com>
2024-06-07 14:08:02 +0200

    Update TabelaWidok

--- TabelaWidok.java
+++ TabelaWidok.java
@@ -2,17 +2,15 @@ import javax.swing.*;
 import javax.swing.plaf.basic.BasicScrollBarUI;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.JTableHeader;
-import java.net.URL;
-
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
-
+import java.net.URL;
 import javax.imageio.ImageIO;
 
-class TabelaWidok extends JFrame
-{
-    TabelaWidok(Tabela tabela)
-    {
+class TabelaWidok extends JFrame {
+    TabelaWidok(Tabela tabela) {
         UIManager.put("TextField.selectionBackground", new Color(124, 252, 0));
         UIManager.put("TextArea.selectionBackground", new Color(124, 252, 0));
         UIManager.put("TextPane.selectionBackground", new Color(124, 252, 0));
@@ -23,14 +21,13 @@ class TabelaWidok extends JFrame
         UIManager.put("Table.selectionBackground", new Color(124, 252, 0));
         UIManager.put("ComboBox.selectionBackground", new Color(124, 252, 0));
 
-        setTitle("Tabela druzyn");
+        setTitle("Tabela drużyn");
         getContentPane().setBackground(new Color(0, 100, 0));
 
         String[] nazwy_kolumn = {"Klub", "Pkt", "RM", "W", "R", "P", "BZ", "BS"};
 
         Object[][] dane = new Object[tabela.druzyny.size()][8];
-        for (int i = 0; i < tabela.druzyny.size(); i++)
-        {
+        for (int i = 0; i < tabela.druzyny.size(); i++) {
             Druzyna d = tabela.druzyny.get(i);
             dane[i][0] = d.getNazwa();
             dane[i][1] = d.getPunkty();
@@ -45,65 +42,55 @@ class TabelaWidok extends JFrame
         JTable wiersze = new JTable(dane, nazwy_kolumn);
         wiersze.setRowHeight(30);
         wiersze.setShowVerticalLines(false);
-        ((DefaultTableCellRenderer)wiersze.getDefaultRenderer(Object.class)).setBorder(BorderFactory.createLineBorder(Color.WHITE));
+        ((DefaultTableCellRenderer) wiersze.getDefaultRenderer(Object.class)).setBorder(BorderFactory.createLineBorder(Color.WHITE));
 
         DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
-        centerRenderer.setHorizontalAlignment( JLabel.CENTER );
-        for (int i=1; i<wiersze.getColumnCount(); i++)
-        {
-            wiersze.getColumnModel().getColumn(i).setCellRenderer( centerRenderer );
+        centerRenderer.setHorizontalAlignment(JLabel.CENTER);
+        for (int i = 1; i < wiersze.getColumnCount(); i++) {
+            wiersze.getColumnModel().getColumn(i).setCellRenderer(centerRenderer);
         }
 
         wiersze.setBackground(new Color(0, 100, 0));
         wiersze.setForeground(Color.WHITE);
-        
 
         JTableHeader header = wiersze.getTableHeader();
         header.setFont(header.getFont().deriveFont(Font.BOLD));
 
-        ((DefaultTableCellRenderer)header.getDefaultRenderer()).setBorder(BorderFactory.createEmptyBorder());
+        ((DefaultTableCellRenderer) header.getDefaultRenderer()).setBorder(BorderFactory.createEmptyBorder());
 
         header.setBackground(new Color(0, 100, 0));
         header.setForeground(Color.WHITE);
-
 
         JPanel panel = new JPanel(new BorderLayout());
         panel.setBackground(new Color(0, 100, 0));
 
         JScrollBar przewijak = new JScrollBar() {
             @Override
-            public void updateUI()
-            {
-                setUI(new BasicScrollBarUI()
-                {
+            public void updateUI() {
+                setUI(new BasicScrollBarUI() {
                     @Override
-                    protected void paintThumb(Graphics g, JComponent c, Rectangle thumbBounds)
-                    {
+                    protected void paintThumb(Graphics g, JComponent c, Rectangle thumbBounds) {
                         g.setColor(Color.WHITE);
                         g.fillRect(thumbBounds.x, thumbBounds.y, thumbBounds.width, thumbBounds.height);
                     }
 
                     @Override
-                    protected void paintTrack(Graphics g, JComponent c, Rectangle trackBounds)
-                    {
+                    protected void paintTrack(Graphics g, JComponent c, Rectangle trackBounds) {
                         g.setColor(new Color(0, 100, 0));
                         g.fillRect(trackBounds.x, trackBounds.y, trackBounds.width, trackBounds.height);
                     }
 
                     @Override
-                    protected JButton createDecreaseButton(int orientation)
-                    {
+                    protected JButton createDecreaseButton(int orientation) {
                         return brak_przycisku();
                     }
-        
+
                     @Override
-                    protected JButton createIncreaseButton(int orientation)
-                    {
+                    protected JButton createIncreaseButton(int orientation) {
                         return brak_przycisku();
                     }
-        
-                    private JButton brak_przycisku()
-                    {
+
+                    private JButton brak_przycisku() {
                         JButton button = new JButton();
                         button.setPreferredSize(new Dimension(0, 0));
                         button.setMinimumSize(new Dimension(0, 0));
@@ -113,7 +100,7 @@ class TabelaWidok extends JFrame
                 });
             }
         };
-        
+
         JPanel naroznik = new JPanel();
         naroznik.setBackground(new Color(0, 100, 0));
 
@@ -124,14 +111,12 @@ class TabelaWidok extends JFrame
         panel.add(przewijalne, BorderLayout.CENTER);
         panel.setSize(300, 150);
         panel.setVisible(true);
-        
-        
+
         JPanel gorny_panel = new JPanel();
         gorny_panel.setLayout(new BorderLayout());
         gorny_panel.setBackground(new Color(0, 100, 0));
 
-        try
-        {
+        try {
             Image obrazek_pilki = ImageIO.read(new URL("https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Soccer_ball.svg/440px-Soccer_ball.svg.png"));
             Image wyskalowany_obrazek_pilki = obrazek_pilki.getScaledInstance(90, 90, Image.SCALE_DEFAULT);
             ImageIcon ikonka_obrazka_pilki = new ImageIcon(wyskalowany_obrazek_pilki);
@@ -139,30 +124,28 @@ class TabelaWidok extends JFrame
             obszar_obrazka_pilki.setHorizontalAlignment(JLabel.CENTER);
             obszar_obrazka_pilki.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
             gorny_panel.add(obszar_obrazka_pilki, BorderLayout.CENTER);
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
         JPanel gorne_przyciski = new JPanel();
         gorne_przyciski.setLayout(new GridLayout(1, 2));
 
-        JButton druzyny = new JButton("Druzyny");
+        JButton druzyny = new JButton("Drużyny");
         druzyny.setBackground(new Color(0, 100, 0));
         druzyny.setForeground(Color.WHITE);
         druzyny.setFont(new Font("Dialog", Font.BOLD, 16));
         gorne_przyciski.add(druzyny);
-        
+
         JButton mecze = new JButton("Mecze");
         mecze.setBackground(new Color(0, 100, 0));
         mecze.setForeground(Color.WHITE);
         mecze.setFont(new Font("Dialog", Font.BOLD, 16));
         gorne_przyciski.add(mecze);
-        
+
         gorny_panel.add(gorne_przyciski, BorderLayout.SOUTH);
-    
-        JButton dodaj = new JButton("Dodaj druzyne");
+
+        JButton dodaj = new JButton("Dodaj drużynę");
 
         JPanel dolny_panel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
         dodaj.setBackground(new Color(0, 100, 0));
@@ -177,6 +160,13 @@ class TabelaWidok extends JFrame
         panel.add(dolny_panel, BorderLayout.SOUTH);
 
         add(panel);
+
+        mecze.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new MeczeWidok(tabela);
+            }
+        });
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
