e4500a13ea9b00670ae502db646f7377eaef64ab
majaomelaniuk <150002963+majaomelaniuk@users.noreply.github.com>
2024-06-21 00:54:11 +0200

    Update DruzynaPodgladWidok.java

--- DruzynaPodgladWidok.java
+++ DruzynaPodgladWidok.java
@@ -1,48 +1,50 @@
 import javax.swing.*;
-
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 class DruzynaPodgladWidok extends JFrame {
-    
+
     public DruzynaPodgladWidok(Druzyna druzyna) {
 
-        setTitle("Drużyna");
+        setTitle("Podgląd Drużyny");
         getContentPane().setBackground(new Color(0, 100, 0));
-        setSize(500, 500);
+        setSize(600, 500);
+        setLocationRelativeTo(null);
         setLayout(new BorderLayout());
 
 
-//------------------ Nazwa --------------------------
-
         JPanel tytulPanel = new JPanel(new BorderLayout());
         tytulPanel.setBackground(new Color(0, 100, 0));
-        tytulPanel.setPreferredSize(new Dimension(tytulPanel.getPreferredSize().width, 100));        
+        tytulPanel.setPreferredSize(new Dimension(tytulPanel.getPreferredSize().width, 100));
         JLabel tytulLabel = new JLabel(druzyna.getNazwa(), SwingConstants.CENTER);
         tytulLabel.setFont(new Font("Arial", Font.BOLD, 24));
         tytulLabel.setForeground(Color.WHITE);
         tytulPanel.add(tytulLabel);
-
         add(tytulPanel, BorderLayout.NORTH);
 
-//------------------- Info ---------------------------
 
         JPanel infoPanel = new JPanel();
         infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.Y_AXIS));
         infoPanel.setBackground(new Color(0, 100, 0));
         infoPanel.setForeground(Color.WHITE);
-        infoPanel.setPreferredSize(new Dimension(200, 100));
-        
-        JLabel trenerLabel = new JLabel("Trener: " + druzyna.getTrener().toString());
+        infoPanel.setFont(new Font("Arial", Font.BOLD, 12));
+
+        JLabel trenerLabel = new JLabel();
         trenerLabel.setForeground(Color.WHITE);
+        trenerLabel.setFont(new Font("Arial", Font.PLAIN, 16));
+
+        trenerLabel.setText("<html><span style='font-family:Arial; font-size:12px;'>Trener: </span>" + druzyna.getTrener().toString() + "</html>");
         infoPanel.add(trenerLabel);
         infoPanel.add(Box.createVerticalStrut(10));
-        
-        JLabel kapitanLabel = new JLabel("Kapitan: " + druzyna.getKapitan().toString() + "   Bramki: " + druzyna.getKapitan().getBramki());
+
+        JLabel kapitanLabel = new JLabel();
         kapitanLabel.setForeground(Color.WHITE);
+        kapitanLabel.setFont(new Font("Arial", Font.PLAIN, 16));
+        kapitanLabel.setText("<html><span style='font-family:Arial; font-size:12px;'>Kapitan: </span>" + druzyna.getKapitan().toString() + "   Bramki: " + druzyna.getKapitan().getBramki() + "</html>");
         infoPanel.add(kapitanLabel);
         infoPanel.add(Box.createVerticalStrut(10));
 
-
+        // Panel z listą piłkarzy
         JPanel pilkarzePanel = new JPanel();
         pilkarzePanel.setBackground(new Color(0, 100, 0));
         pilkarzePanel.setForeground(Color.WHITE);
@@ -51,12 +53,14 @@ class DruzynaPodgladWidok extends JFrame {
         for (Pilkarz pilkarz : druzyna.getPilkarze()) {
             String pilkarzInfo = pilkarz.toString();
             if (pilkarz instanceof Bramkarz) {
-                pilkarzInfo += "   Obrony: " + Integer.toString(((Bramkarz) pilkarz).getLiczbaObron());
+                pilkarzInfo += "   Obrony: " + ((Bramkarz) pilkarz).getLiczbaObron();
             } else {
                 pilkarzInfo += "   Bramki: " + pilkarz.getBramki();
             }
-            JLabel pilkarzLabel = new JLabel(pilkarzInfo);
+            JLabel pilkarzLabel = new JLabel();
             pilkarzLabel.setForeground(Color.WHITE);
+            pilkarzLabel.setFont(new Font("Arial", Font.PLAIN, 16));
+            pilkarzLabel.setText("<html>" + pilkarzInfo + "</html>");
             pilkarzePanel.add(pilkarzLabel);
         }
 
@@ -65,7 +69,6 @@ class DruzynaPodgladWidok extends JFrame {
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 
         infoPanel.add(scrollPane);
-
         add(infoPanel, BorderLayout.CENTER);
 
         setVisible(true);
